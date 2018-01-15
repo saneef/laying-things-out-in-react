adcd266287650a86bab7b033e84906d0b233c9c3
Saneef Ansari <saneef@users.noreply.github.com>
2018-01-15 11:54:38 +0530

    adds global base styles

--- src/Wrapper/Wrapper.jsx
+++ src/Wrapper/Wrapper.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { injectGlobal } from 'styled-components'
 
-export default styled.div`
-  font-family: sans-serif;
-
-  * {
+const baseStyles = () => injectGlobal`
+* {
     margin: 0;
   }
 
   * + * {
     margin-top: 1em;
   }
+
+  html, body {
+    margin: 0;
+  }
+`;
+
+const Wrapper= ({className, children}) => {
+  baseStyles();
+
+  return <div className={className}>{children}</div>
+}
+
+export default styled(Wrapper)`
+  font-family: sans-serif;
 `;
